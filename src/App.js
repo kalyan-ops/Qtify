fd4eda0962a607a60143a35caa5fa519889e60cc
KARTIK5799 <thekartiksapkal@gmail.com>
2023-11-17 10:43:30 +0530

    cd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     hello
-    </div>
+ <>
+ hello
+ </>
   );
 }
 
