8a78bfed51185bdada91074886f88e920f74cc79
KARTIK5799 <thekartiksapkal@gmail.com>
2023-11-27 22:48:21 +0530

    g

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -24,7 +24,7 @@ const Carousel=({data,renderComponent})=>{
             style={{padding:"0px 20px"}}
             initialSlide={0}
             modules={[Navigation]}
-            slidesPerView={'auto'}
+            slidesPerView={8}
             spaceBetween={40}
             allowTouchMove
             >
